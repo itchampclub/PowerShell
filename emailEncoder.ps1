5d1085a842eb07a5b41aef7ce071fbe0bd8ccc4c
Tommy Becker <tommy.becker@gmail.com>
2013-06-05 15:37:28 -0500

    Create emailEncoder.ps1

---
+++ emailEncoder.ps1
@@ -0,0 +1,4 @@
+$email = "tbecker@noblecorp.com"
+Write-Host -NoNewline "[string](0..$($email.Length-1)|%{[char][int](46+("""
+[int[]][char[]]$email | foreach{Write-Host -NoNewline "$($_-46)".Replace(0,"00").Substring(0,2)}
+Write-Host -NoNewline """).substring((`$_*2),2))})-replace "" """
