b844bdf5e265172184fd9db5bb94b15fb95cec89
Tommy Becker <MockMyBeret@gmail.com>
2013-07-23 10:26:15 -0500

    Some new stuff

---
+++ Get-proxyaddresses.ps1
@@ -0,0 +1,14 @@
+﻿$users = Get-ADUser -Filter * -Properties proxyaddresses
+$maxProxy = $users | %{$_.proxyaddresses.count} | Sort-Object | Select-Object -Last 1
+foreach ($u in $users)
+{
+$proxyaddress = [ordered]@{}
+$proxyaddress.Add("User",$u.name)
+for ($i=0; $i -le $maxProxy; $i++)
+{
+$proxyaddress.add("proxyaddress_$i",$u.proxyaddresses[$I])
+} #end for
+[pscustomobject]$proxyAddress | Export-Csv -Path h:\fso\proxyaddresses.csv -NoTypeInformation –Append -Force
+#$proxyaddress #just to see if it made it this far.
+Remove-Variable -Name proxyAddress
+}
---
+++ get-processandowner.ps1
@@ -0,0 +1,6 @@
+﻿function get-processandowner ($computer)
+{
+    $owners=@{}
+    gwmi win32_process -ComputerName $computer | % {$owners[$_.handle] = $_.getowner().user}
+    get-process -ComputerName $computer | select *,@{l="Owner";e={$owners[$_.id.tostring()]}}
+}
