0c1e7b9dcabdfb17420e4da6d3b7ed40898746e5
Tommy Becker <MockMyBeret@gmail.com>
2013-08-06 11:01:09 -0500

    initial

--- Get-proxyaddresses.ps1
+++ Get-proxyaddresses.ps1
@@ -1,5 +1,8 @@
-﻿$users = Get-ADUser -Filter * -Properties proxyaddresses
-$maxProxy = $users | %{$_.proxyaddresses.count} | Sort-Object | Select-Object -Last 1
+﻿[CmdletBinding(SupportsShouldProcess=$true)]
+Param()
+
+$users = Get-ADUser -Filter * -Properties proxyaddresses
+$maxProxy = $users | %{([array]$_.proxyaddresses).GetUpperBound(0)} | Sort | Select -Last 1
 foreach ($u in $users)
 {
 $proxyaddress = [ordered]@{}
@@ -9,6 +12,6 @@ for ($i=0; $i -le $maxProxy; $i++)
 $proxyaddress.add("proxyaddress_$i",$u.proxyaddresses[$I])
 } #end for
 [pscustomobject]$proxyAddress | Export-Csv -Path h:\fso\proxyaddresses.csv -NoTypeInformation –Append -Force
-#$proxyaddress #just to see if it made it this far.
+Write-Verbose $proxyaddress | Select * #just to see if it made it this far.
 Remove-Variable -Name proxyAddress
 }
---
+++ get-IPsAndPingHosts.ps1
@@ -0,0 +1,3 @@
+﻿$systems = Import-Csv 'H:\systems without client.csv' -Header "System","OS","AD_Site"
+$systems | %{ Add-Member -inputObject $_ -passThru -type NoteProperty -name IP_From_Name -Value $(try{[system.net.dns]::GetHostByName($_.System).AddressList.GetValue(0).ToString()}catch{""}) -Force}
+$systems | %{ Add-Member -inputObject $_ -passThru -type NoteProperty -name Pingable -Value $(try{(New-Object System.Net.NetworkInformation.Ping).send([System.Net.IPAddress]$_.IP_From_Name).Status.ToString()}catch{""}) -Force}
