edb6e509821a45bc7255a49db4e764ac882e0fd6
Tommy Becker <MockMyBeret@gmail.com>
2013-09-11 07:34:01 -0500

    few additions

---
+++ Add-FunctionFromScript.ps1
@@ -0,0 +1,3 @@
+﻿$ParserTokens = $null
+$ParserErrors = $null
+$AST = [System.Management.Automation.Language.Parser]::ParseFile("h:\github\powershell\Get-DellWarrantyInfo.ps1",[ref]$ParserTokens,[ref]$ParserErrors)
