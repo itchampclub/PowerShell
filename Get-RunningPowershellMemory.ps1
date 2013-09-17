8a96e844d1b7bd1a613383711ec150df7d2bca74
Tommy Becker <MockMyBeret@gmail.com>
2013-09-17 17:48:45 -0500

    init

--- Get-RunningPowershellMemory.ps1
+++ Get-RunningPowershellMemory.ps1
@@ -3,6 +3,7 @@ foreach($Process in $Processes)
 {
     $Obj = New-Object psobject
     $Obj | Add-Member -MemberType NoteProperty -Name Name -Value $Process.Name
+    $ExecutionContext.Host.EnterNestedPrompt()
     $Obj | Add-Member -MemberType NoteProperty -Name WorkingSetInMegs -Value (($Process.Group|Measure-Object WorkingSet -Sum).Sum/1MB)
     $Obj    
 }
---
+++ Get-TotalWSMemory.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.Synopsis
+Get aggregate working set memory utilization for processes.
+.EXAMPLE
+Get-Process | sort processname -Unique | select ProcessName | .\Get-TotalWSMemory.ps1 | ft name, @{n='WS in MB';e={"{0:N2} MB" -f $($_.WorkingSetTotal/1MB)};align='right'}
+#>
+param(
+[Parameter(ValueFromPipelineByPropertyName=$true,
+ValueFromPipeline=$true)]
+[string[]]$ProcessName = "*"
+)
+begin{}
+process
+{
+$Processes = get-process $ProcessName | Group-Object -Property ProcessName
+foreach($Process in $Processes)
+{
+    $Obj = New-Object psobject
+    $Obj | Add-Member -MemberType NoteProperty -Name Name -Value $Process.Name
+    $Obj | Add-Member -MemberType NoteProperty -Name WorkingSetTotal -Value (($Process.Group|Measure-Object WorkingSet -Sum).Sum)
+    $Obj
+}
+}
+end{}
