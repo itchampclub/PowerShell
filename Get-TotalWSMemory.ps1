026b89ade6295f36b4403deba56b1ac0cf6a364a
Tommy Becker <MockMyBeret@gmail.com>
2013-09-18 17:07:58 -0500

    update
    update to include more examples

--- Get-TotalWSMemory.ps1
+++ Get-TotalWSMemory.ps1
@@ -3,6 +3,10 @@
 Get aggregate working set memory utilization for processes.
 .EXAMPLE
 Get-Process | sort processname -Unique | select ProcessName | .\Get-TotalWSMemory.ps1 | ft name, @{n='WS in MB';e={"{0:N2} MB" -f $($_.WorkingSetTotal/1MB)};align='right'}
+To get the total memory for all running processes.
+.EXAMPLE
+Get-Process | sort processname -Unique | select ProcessName | .\Get-TotalWSMemory.ps1 | sort workingsettotal -desc | select -First 10 | ft name, @{n='WS in MB';e={"{0:N2} MB" -f $($_.WorkingSetTotal/1MB)};align='right'}
+To get the top 10.
 #>
 param(
 [Parameter(ValueFromPipelineByPropertyName=$true,
