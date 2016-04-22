a97d0f0b23e793513da0dd77e82257f511e0798f
Tommy Becker <tommy.becker@gmail.com>
2016-04-22 10:38:12 -0500

    Update emailEncoder.ps1

--- emailEncoder.ps1
+++ emailEncoder.ps1
@@ -1,6 +1,9 @@
+function Get-EncodedEmail
+{
 ﻿param(
 $email = "tommy.becker@gmail.com"
 )
 Write-Host -NoNewline "[string](0..$($email.Length-1)|%{[char][int](46+("""
 [int[]][char[]]$email | foreach{Write-Host -NoNewline "$($_-46)".Replace(0,"00").Substring(0,2)}
 Write-Host -NoNewline """).substring((`$_*2),2))})-replace "" """
+}
